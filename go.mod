8de413f1bba7aa434604707b8b827d738885e561
sminamot <shosfs@gmail.com>
2020-01-30 23:17:18 +0900

    :heavy_plus_sign: use S3

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/sminamot/line-googlehome-bot
 go 1.13
 
 require (
+	github.com/aws/aws-sdk-go v1.28.9
 	github.com/barnybug/go-cast v0.0.0-20190910160619-d2aa97f56d4e // indirect
 	github.com/evalphobia/google-home-client-go v0.1.1
 	github.com/evalphobia/google-tts-go v0.3.0 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/line/line-bot-sdk-go v6.4.0+incompatible
 	github.com/stretchr/testify v1.4.0 // indirect
-	github.com/yosssi/go-voicetext v0.0.0-20180801114841-801caf53e208
 	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/evalphobia/google-home-client-go/googlehome"
 	"github.com/line/line-bot-sdk-go/linebot"
-	"github.com/yosssi/go-voicetext"
 )
+
+const voinceTextWebAPIURL = "https://api.voicetext.jp/v1/tts"
 
 func main() {
 	bot, err := linebot.New(
@@ -40,10 +48,12 @@ func main() {
 					if err == nil {
 						userName = profile.DisplayName
 					}
-					if err := saveWav(message.Text, userName); err != nil {
-						log.Print(err)
-						continue
-					}
+					/*
+						if err := saveWav(message.Text, userName); err != nil {
+							log.Print(err)
+							continue
+						}
+					*/
 					if err := speak(message.Text, userName); err != nil {
 						log.Print(err)
 					}
@@ -80,11 +90,80 @@ func speak(text, user string) error {
 		text = fmt.Sprintf("%sからのメッセージが届きました。。。%s", user, text)
 	}
 
+	url, err := getVoiceURL(text)
+	if err != nil {
+		return err
+	}
+
 	// return cli.Play(fmt.Sprintf("http://127.0.0.1:%s/voice/line.wav", os.Getenv("PORT")))
-	url := fmt.Sprintf("http://%s/voice/line.wav", os.Getenv("MEDIA_DOMAIN"))
+	// url := fmt.Sprintf("http://%s/voice/line.wav", os.Getenv("MEDIA_DOMAIN"))
 	return cli.Play(url)
 }
 
+func getVoiceURL(text string) (string, error) {
+	v := url.Values{}
+	v.Add("text", text)
+	v.Add("speaker", "hikari")
+	v.Add("format", "mp3")
+	v.Add("volume", "200")
+
+	res, err := requestVoiceTextWebAPI(os.Getenv("VOICETEXT_API_KEY"), v)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return "", errors.New("failed to request VoiceText Web API")
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1")},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	bucket := aws.String(os.Getenv("AWS_S3_BUCKET"))
+	filename := aws.String(fmt.Sprint(time.Now().UnixNano()) + ".mp3")
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: bucket,
+		Key:    filename,
+		Body:   res.Body,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	svc := s3.New(sess)
+
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: bucket,
+		Key:    filename,
+	})
+	url, err := req.Presign(30 * time.Second) // 有効期限を指定して署名付きURLを取得
+	if err != nil {
+		fmt.Println("failed to S3 sign request", err)
+	}
+
+	return url, nil
+}
+
+func requestVoiceTextWebAPI(key string, values url.Values) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodPost, voinceTextWebAPIURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.URL.RawQuery = values.Encode()
+	req.SetBasicAuth(key, "")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	return http.DefaultClient.Do(req)
+}
+
+/*
 func saveWav(text, user string) error {
 	c := voicetext.NewClient(os.Getenv("VOICETEXT_API_KEY"), nil)
 	result, err := c.TTS(fmt.Sprintf("%sからのメッセージが届きました。%s", user, text), &voicetext.TTSOptions{
@@ -109,3 +188,4 @@ func saveWav(text, user string) error {
 	_, err = f.Write(result.Sound)
 	return err
 }
+*/
